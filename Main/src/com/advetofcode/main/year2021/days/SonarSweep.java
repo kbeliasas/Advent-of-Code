ba5269a3162266886b3e3675e2a4ec2ce2b97a51
Karolis Beliasas (nsj7529) <karolis.beliasas@teliacompany.com>
2021-12-01 16:46:04 +0200

    Day1

--- Main/src/com/advetofcode/main/year2021/days/SonarSweep.java
+++ Main/src/com/advetofcode/main/year2021/days/SonarSweep.java
@@ -1,15 +1,25 @@
 package com.advetofcode.main.year2021.days;
 
+import static java.lang.Long.parseLong;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.advetofcode.main.CustomFileReader;
 
 public class SonarSweep {
     private static CustomFileReader customFileReader = new CustomFileReader();
 
-    private static String path = "/Users/nsj7529/repository/Advent-of-Code/Main/src/com/advetofcode/main/year2021/resources/Day1_Par1.txt";
+    private static String path1 = "/Users/nsj7529/repository/Advent-of-Code/Main/src/com/advetofcode/main/year2021/resources/Day1_Par1_test.txt";
+    private static String path2 = "/Users/nsj7529/repository/Advent-of-Code/Main/src/com/advetofcode/main/year2021/resources/Day1_Par1.txt";
 
+    // PART1
     public static void main(String[] args) throws IOException {
+        //part1(path1);
+        part2(path2);
+    }
+    // PART2
+    private static void part1(String path) throws IOException {
         var rawDataList = customFileReader.readFile(path);
 
         var count = 0;
@@ -17,10 +27,36 @@ public class SonarSweep {
         Long last = null;
 
         for (var line : rawDataList) {
-            if (last != null && Long.parseLong(line) > last) {
+            if (last != null && parseLong(line) > last) {
                 count++;
             }
-            last = Long.parseLong(line);
+            last = parseLong(line);
+        }
+
+        System.out.println(count);
+    }
+
+    private static void part2(String path) throws IOException {
+        var rawDataList = customFileReader.readFile(path);
+
+        var modifiedDataList = new ArrayList<Long>();
+
+        for (int i = 2; i < rawDataList.size(); i++) {
+            var newLong = parseLong(rawDataList.get(i-2)) + parseLong(rawDataList.get(i-1)) + parseLong(rawDataList.get(i));
+            modifiedDataList.add(newLong);
+        }
+
+        //System.out.println(modifiedDataList);
+
+        var count = 0;
+
+        Long last = null;
+
+        for (var line : modifiedDataList) {
+            if (last != null && line > last) {
+                count++;
+            }
+            last = line;
         }
 
         System.out.println(count);
